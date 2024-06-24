5819635e90f0aae4ce90f240c37db54d14536862
MaddelSanthi <168314503+MaddelSanthi@users.noreply.github.com>
2024-06-24 13:21:44 +0530

    Delete aws_account directory

--- aws_account/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "account_id" {
-  description = "Account ID"
-  value       = aws_organizations_account.account.id
-}
--- aws_account/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "scalr_aws_secret_key" {}
-variable "scalr_aws_access_key" {}
-variable "account_name" {}
-variable "owner_email" {}
-variable "region" {
-  default = "us-east-1"
-}
