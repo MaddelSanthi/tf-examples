fff24da4b131db70d652fd65533cbb6c8d0e728d
MaddelSanthi <168314503+MaddelSanthi@users.noreply.github.com>
2024-06-24 21:31:01 +0530

    Update main.tf

--- terraform-aws-sg/main.tf
+++ terraform-aws-sg/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "scalr" {
-  ami                    = "ami-0abcdef1234567890"  # Replace with a valid AMI ID
+  #ami                    = "ami-0abcdef1234567890"  # Replace with a valid AMI ID
   instance_type          = "t2.micro"
   subnet_id              = "subnet-12345678"
   vpc_security_group_ids = ["sg-12345678"]
