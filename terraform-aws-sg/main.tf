7571187cc0bc359ce5d2789a078ac9084c13207d
MaddelSanthi <168314503+MaddelSanthi@users.noreply.github.com>
2024-06-25 09:19:41 +0530

    Update main.tf

--- terraform-aws-sg/main.tf
+++ terraform-aws-sg/main.tf
@@ -1,15 +1,3 @@
-/*provider "aws" {
-  region = "us-east-1"
-}
-
-resource "aws_instance" "scalr" {
-  ami                    = "i-05a1467996ef9db97"  # Replace with a valid AMI ID
-  instance_type          = "t2.micro"
-  subnet_id              = "subnet-12345678"
-  vpc_security_group_ids = ["sg-12345678"]
-  key_name               = "my-keypair"
-  count                  = 1
-}*/
 terraform {
   required_providers {
     aws = {
@@ -22,7 +10,6 @@ terraform {
 
 provider "aws" {
   region  = "us-east-1"
-  //profile = "jack.roper"
 }
 
 resource "aws_instance" "example_server" {
@@ -30,6 +17,6 @@ resource "aws_instance" "example_server" {
   instance_type = "t2.micro"
 
   tags = {
-    Name = "JacksBlogExample"
+    Name = "scalrec2"
   }
 }
